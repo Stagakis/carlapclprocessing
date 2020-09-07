595e61922a83548868499fe51680cf43ee66c810
Stagakis <nick-stag@hotmail.com>
2020-09-07 14:34:27 +0300

    Refactor Camera code to fit new Event handling schema

---
+++ src/Camera.cpp
@@ -0,0 +1,110 @@
+#include <Camera.h>
+
+Camera::Camera(glm::vec3 position, glm::vec3 up , float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+{
+    Position = position;
+    WorldUp = up;
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+}
+
+Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+{
+    Position = glm::vec3(posX, posY, posZ);
+    WorldUp = glm::vec3(upX, upY, upZ);
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+}
+
+glm::mat4 Camera::GetViewMatrix() const
+{
+    return glm::lookAt(Position, Position + Front, Up);
+}
+
+void Camera::ProcessMouseScroll(float yoffset)
+{
+    Zoom -= (float)yoffset;
+    if (Zoom < 1.0f)
+        Zoom = 1.0f;
+    if (Zoom > 45.0f)
+        Zoom = 45.0f;
+}
+
+void Camera::updateCameraVectors()
+{
+    // calculate the new Front vector
+    glm::vec3 front;
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    Front = glm::normalize(front);
+    // also re-calculate the Right and Up vector
+    Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+    Up    = glm::normalize(glm::cross(Right, Front));
+}
+
+void Camera::OnKeyboardEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(glfwGetKey(window, GLFW_KEY_GRAVE_ACCENT) == GLFW_PRESS) {
+        if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_NORMAL) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            cameraIsActive = true;
+        } else {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+            cameraIsActive = false;
+        }
+    }
+}
+
+void Camera::OnMouseEvent(GLFWwindow* window, double xpos, double ypos) {
+    static double lastX = xpos;
+    static double lastY = ypos;
+
+    if(cameraIsActive) {
+
+        double xoffset = xpos - lastX;
+        double yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+        lastX = xpos;
+        lastY = ypos;
+
+        xoffset *= MouseSensitivity;
+        yoffset *= MouseSensitivity;
+
+        Yaw   += xoffset;
+        Pitch += yoffset;
+
+        if (Pitch > 89.0f)
+            Pitch = 89.0f;
+        if (Pitch < -89.0f)
+            Pitch = -89.0f;
+
+        updateCameraVectors();
+    }
+}
+
+void Camera::OnFrameUpdate(GLFWwindow* window, float deltaTime) {
+    float velocity = MovementSpeed * deltaTime;
+
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        Position += Front * velocity;
+    }
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        Position -= Right * velocity;
+    }
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        Position -= Front * velocity;
+    }
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        Position += Right * velocity;
+    }
+    if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+        Position += glm::vec3(0.0f, 1.0f, 0.0f) * velocity;
+    }
+    if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+        Position -= glm::vec3(0.0f, 1.0f, 0.0f) * velocity;
+    }
+
+}
--- src/WindowEventPublisher.cpp
+++ src/WindowEventPublisher.cpp
@@ -1,40 +1,50 @@
-//
-// Created by stagakis on 4/9/20.
-//
-
 #include "WindowEventPublisher.h"
 
 std::vector<IntWindowEventListener*> WindowEventPublisher::keyboardListeners = std::vector<IntWindowEventListener*>();
 std::vector<IntWindowEventListener*> WindowEventPublisher::mouseListeners = std::vector<IntWindowEventListener*>();
 std::vector<IntWindowEventListener*> WindowEventPublisher::scrollListeners = std::vector<IntWindowEventListener*>();
+std::vector<IntWindowEventListener*> WindowEventPublisher::frameUpdateListeners = std::vector<IntWindowEventListener*>();
 
 
+//*//
 void WindowEventPublisher::addKeyboardListener(IntWindowEventListener& listener) {
     keyboardListeners.push_back(&listener);
 }
+//*/
 
-void WindowEventPublisher::addMouseListener(IntWindowEventListener* listener) {
-    mouseListeners.push_back(listener);
+void WindowEventPublisher::addMouseListener(IntWindowEventListener&  listener) {
+    mouseListeners.push_back(&listener);
 }
 
-void WindowEventPublisher::addScrollListener(IntWindowEventListener* listener) {
-    scrollListeners.push_back(listener);
+void WindowEventPublisher::addScrollListener(IntWindowEventListener&  listener) {
+    scrollListeners.push_back(&listener);
 }
 
-void WindowEventPublisher::keyboardCallback(int key, int scancode, int action, int mods) {
+void WindowEventPublisher::addFrameUpdateListener(IntWindowEventListener &listener) {
+    frameUpdateListeners.push_back(&listener);
+}
+
+void WindowEventPublisher::keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     for(auto & keyboardListener : keyboardListeners){
-        keyboardListener->OnKeyboardEvent(key, scancode, action, mods);
+        keyboardListener->OnKeyboardEvent(window, key, scancode, action, mods);
     }
 }
 
-void WindowEventPublisher::mouseCallback(double xpos, double ypos) {
+void WindowEventPublisher::mouseCallback(GLFWwindow* window, double xpos, double ypos) {
     for(auto & mouseListener : mouseListeners){
-        mouseListener->OnMouseEvent(xpos, ypos);
+        mouseListener->OnMouseEvent(window, xpos, ypos);
     }
 }
 
-void WindowEventPublisher::scrollCallback(double xoffset, double yoffset) {
+void WindowEventPublisher::scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
     for(auto & scrollListener : scrollListeners){
-        scrollListener->OnScrollEvent(xoffset, yoffset);
+        scrollListener->OnScrollEvent(window, xoffset, yoffset);
     }
 }
+
+void WindowEventPublisher::notifyFrameUpdate(GLFWwindow* window, float deltaTime) {
+    for(auto & frameUpdateListener : frameUpdateListeners){
+        frameUpdateListener->OnFrameUpdate(window, deltaTime);
+    }
+}
+
