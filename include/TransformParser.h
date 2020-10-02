526cc4ad48adcf05339895fbf8a8a6fb529e44d6
Stagakis <nick-stag@hotmail.com>
2020-10-02 16:55:25 +0300

    Start to write camera pos parser

---
+++ include/TransformParser.h
@@ -0,0 +1,20 @@
+#include "glm/glm.hpp"
+#include <string>
+#include <vector>
+
+#ifndef MYOPENGL_TRANSFORMPARSER_H
+#define MYOPENGL_TRANSFORMPARSER_H
+
+
+class TransformParser {
+public:
+    std::vector<glm::vec3> rgbPos;
+    std::vector<glm::vec3> lidarPos;
+    std::vector<glm::vec3> rgbRot;
+    std::vector<glm::vec3> lidarRot;
+
+    TransformParser(std::string filename);
+};
+
+
+#endif //MYOPENGL_TRANSFORMPARSER_H
