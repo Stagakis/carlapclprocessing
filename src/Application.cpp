d3a0f203e7fd9141bc6926d213f2cde5eae7034a
Stagakis <nick-stag@hotmail.com>
2021-02-08 14:11:31 +0200

    move main() to main.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,7 +5,6 @@
 #include <future>
 #include <iostream>
 
-
 int Application::AppMain() {
     ShaderLoader ourShader("vertexShader.shader", "fragmentShader.shader");
     ShaderLoader postProcessShader("vertexShader.shader", "KNearest_fragment.shader");
@@ -193,42 +192,4 @@ void Application::setUpWindowEventHandlers() {
     WindowEventPublisher::addMouseListener(camera);
     WindowEventPublisher::addScrollListener(camera);
     WindowEventPublisher::addFrameUpdateListener(camera);
-}
-
-int main()
-{
-    auto app = Application();
-
-    app.window = createGlfwWindow(SCR_WIDTH, SCR_HEIGHT, "CPSoS", false);
-    if(app.window == NULL) return -1;
-    glfwSetKeyCallback(app.window, [](GLFWwindow* window, int key, int scancode, int action, int mods){ WindowEventPublisher::keyboardCallback(window, key,scancode,action,mods);});
-    glfwSetCursorPosCallback(app.window, [](GLFWwindow* window, double xpos, double ypos){ WindowEventPublisher::mouseCallback(window, xpos, ypos);});
-    glfwSetScrollCallback(app.window, [](GLFWwindow* window, double xoffset, double yoffset){ WindowEventPublisher::scrollCallback(window, xoffset, yoffset);});
-    glfwSetFramebufferSizeCallback(app.window, [](GLFWwindow* window, int width, int height){glViewport(0, 0, width, height);});
-
-    glfwMakeContextCurrent(app.window);
-
-    //glfwSetInputMode(app.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    // Setup Dear ImGui context /////////////////////
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-    // Setup Platform/Renderer bindings
-    ImGui_ImplGlfw_InitForOpenGL(app.window, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
-
-    app.setUpWindowEventHandlers();
-
-    app.Initialization();
-    return app.AppMain();
 }
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "Application.h"
+#include "WindowEventPublisher.h"
+#include "helpers.h"
+
+int main()
+{
+    auto app = Application();
+
+    app.window = createGlfwWindow(SCR_WIDTH, SCR_HEIGHT, "CPSoS", false);
+    if(app.window == NULL) return -1;
+    glfwSetKeyCallback(app.window, [](GLFWwindow* window, int key, int scancode, int action, int mods){ WindowEventPublisher::keyboardCallback(window, key,scancode,action,mods);});
+    glfwSetCursorPosCallback(app.window, [](GLFWwindow* window, double xpos, double ypos){ WindowEventPublisher::mouseCallback(window, xpos, ypos);});
+    glfwSetScrollCallback(app.window, [](GLFWwindow* window, double xoffset, double yoffset){ WindowEventPublisher::scrollCallback(window, xoffset, yoffset);});
+    glfwSetFramebufferSizeCallback(app.window, [](GLFWwindow* window, int width, int height){glViewport(0, 0, width, height);});
+
+    glfwMakeContextCurrent(app.window);
+
+    //glfwSetInputMode(app.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Setup Dear ImGui context /////////////////////
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(app.window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+
+    app.Initialization();
+    app.setUpWindowEventHandlers();
+
+    return app.AppMain();
+}
