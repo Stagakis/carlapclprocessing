7025967787c53fed0e3009be467bcc7a89f5040a
Stagakis <nick-stag@hotmail.com>
2021-02-11 20:03:30 +0200

    Minor deletes

--- src/Ego.cpp
+++ src/Ego.cpp
@@ -80,13 +80,7 @@ Ego::Ego(std::string resources_folder) {
     for(size_t i = 0 ; i < files.size(); i++){
         pointclouds[i].translation = glm::vec3(imu_carla_to_opengl_coords * glm::vec4(transformData.lidarPos[i], 1.0f));
 
-        auto x_test = glm::vec3(imu_carla_to_opengl_coords * glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
-        auto y_test = glm::vec3(imu_carla_to_opengl_coords * glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
-        auto z_test = glm::vec3(imu_carla_to_opengl_coords * glm::vec4(0.0f, 0.0f, 1.0f, 1.0f));
-
-
-        auto test1 = glm::vec3(-transformData.lidarRot[i][1], transformData.lidarRot[i][0], -transformData.lidarRot[i][2]);
-        pointclouds[i].ypr = test1; // glm::vec3(-transformData.lidarRot[i][1], transformData.lidarRot[i][0], -transformData.lidarRot[i][2]); // roll is minus because we look at the -z axis
+        pointclouds[i].ypr = glm::vec3(-transformData.lidarRot[i][1], transformData.lidarRot[i][0], -transformData.lidarRot[i][2]);; // glm::vec3(-transformData.lidarRot[i][1], transformData.lidarRot[i][0], -transformData.lidarRot[i][2]); // roll is minus because we look at the -z axis
         pointclouds[i].updateModelMatrix();
     }
 
